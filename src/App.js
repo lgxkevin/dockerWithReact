edb4c55ebd7df7d52e6d678d8cb167ded1877c93
lgxkevin <lgx9587@gmail.com>
2019-09-11 13:10:20 +1200

    testing ci/cd

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           New feature branch <code>src/App.js</code> and save to reload.
+          New version
         </p>
         <a
           className="App-link"
